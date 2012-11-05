350930302d31586302d759055fa9a2b520479d2b
Mário Kapusta <mariokapustaa@gmail.com>
2012-11-05 02:12:17 +0100

    adding new functionalities
    functionalities about finding lines on screen

---
+++ src/org/find/trafic/signes/findTraficSignes.java
@@ -0,0 +1,36 @@
+package org.find.trafic.signes;
+
+/**
+ * 
+ * class findTraficSignes is responsible for finding trafic signes in this project
+ * 
+ * @author majko
+ *
+ */
+
+public class findTraficSignes {
+	  
+	  /**
+	   * constructor
+	   */
+	  
+	  public findTraficSignes(){
+		  
+	  }
+	  
+	  /**
+	   * mathod to find the red color on the screen
+	   */
+	  
+	  public static void findRed(){
+	      
+	  }
+	  
+	  /**
+	   * mathod to find the lines on the screen
+	   */
+	  
+	  public static void findLines(){
+	      
+	  }
+}
--- src/org/object/recognition/ActivityFunctionality.java
+++ src/org/object/recognition/ActivityFunctionality.java
@@ -32,8 +32,12 @@ class ActivityFunctionality extends BaseClass {
     private Mat mRgba;
     private Mat mGraySubmat;
     private Mat mIntermediateMat;
+    private Mat lines;
 	private Bitmap mBitmap;
 	private int mViewMode;
+	private int lineGap;
+	private int minLineSize;
+	private int threshold;
 
     public ActivityFunctionality(Context context) {
         super(context);
@@ -46,11 +50,13 @@ class ActivityFunctionality extends BaseClass {
         	// initialize Mats before usage
         	mYuv = new Mat(getFrameHeight() + getFrameHeight() / 2, getFrameWidth(), CvType.CV_8UC1);
         	mGraySubmat = mYuv.submat(0, getFrameHeight(), 0, getFrameWidth());
-
+        	
         	mRgba = new Mat();
         	mIntermediateMat = new Mat();
+        	lines = new Mat();
 
         	mBitmap = Bitmap.createBitmap(previewWidth, previewHeight, Bitmap.Config.ARGB_8888); 
+        	
     	    }
 	}
 
@@ -84,6 +90,8 @@ class ActivityFunctionality extends BaseClass {
 	 * processFrame method
 	 * - it depends on selected value from menu
 	 * - work intime, with camera view
+	 * lines detect :
+	 * http://docs.opencv.org/modules/imgproc/doc/feature_detection.html#houghlinesp
 	 */
 	
     @Override
@@ -107,13 +115,24 @@ class ActivityFunctionality extends BaseClass {
             Core.putText(mRgba, "CANNY mode", new Point(10, 100), 3/* CV_FONT_HERSHEY_COMPLEX */, 2, new Scalar(255, 0, 0, 255), 3);
             break;
         case VIEW_MODE_TRAFFIC_SIGNES:
-        	Imgproc.cvtColor(mYuv, mRgba, Imgproc.COLOR_YUV420sp2RGB, 4);
-        	//TODO new class TraficSignes();
+        	threshold = 50;
+        	minLineSize = 50;
+        	lineGap = 10;
+        	Imgproc.Canny(mGraySubmat, mIntermediateMat, 80, 100);
+        	Imgproc.HoughLinesP(mIntermediateMat, lines, 1, Math.PI/180, threshold, minLineSize, lineGap);
+            Imgproc.cvtColor(mIntermediateMat, mRgba, Imgproc.COLOR_GRAY2BGRA, 4);
+        	for (int x = 0; x < lines.cols(); x++){
+        		double[] vec = lines.get(0, x);
+                double x1 = vec[0], y1 = vec[1], x2 = vec[2], y2 = vec[3];
+                Point start = new Point(x1, y1);
+                Point end = new Point(x2, y2);
+                Core.line(mRgba, start, end, new Scalar(255,0,0), 3);
+            }
             break;
         }
 
         Bitmap bmp = mBitmap;
-
+        
         try {
             Utils.matToBitmap(mRgba, bmp);
         } catch(Exception e) {
